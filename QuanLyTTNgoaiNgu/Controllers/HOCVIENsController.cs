5ecb15d1f855f559505abe2c4f69a1464795eb3f
TinDuong <duongductin02@gmail.com>
2025-07-19 06:28:41 +0700

    Xong hoc vien (admin)

--- QuanLyTTNgoaiNgu/Controllers/HOCVIENsController.cs
+++ QuanLyTTNgoaiNgu/Controllers/HOCVIENsController.cs
@@ -28,110 +28,166 @@ namespace QuanLyTTNgoaiNgu.Controllers
         // GET: HOCVIENs/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var hOCVIEN = await _context.HOCVIEN
-                .FirstOrDefaultAsync(m => m.MaHocVien == id);
-            if (hOCVIEN == null)
-            {
-                return NotFound();
-            }
+            var hocVien = await _context.HOCVIEN
+                              .FirstOrDefaultAsync(m => m.MaHocVien == id);
+            if (hocVien == null) return NotFound();
 
-            return View(hOCVIEN);
+            return View(hocVien);
         }
+
+        // GET: HOCVIENs/DetailsWithHistory/5
+        public async Task<IActionResult> DetailsWithHistory(int? id)
+        {
+            if (id == null) return NotFound();
+            var hocVien = await _context.HOCVIEN.FindAsync(id);
+            if (hocVien == null) return NotFound();
+
+            ViewBag.LichSuDK = await _context.PHIEUDANGKY
+                .Include(d => d.LOPHOC)
+                .Where(d => d.MaHocVien == id)
+                .ToListAsync();
+
+            ViewBag.KetQua = await _context.KETQUAHOCTAP
+                .Include(k => k.LOPHOC)
+                .Where(k => k.MaHocVien == id)
+                .ToListAsync();
+
+            return View(hocVien);
+        }
+
+
 
         // GET: HOCVIENs/Create
         public IActionResult Create()
         {
+            // Optionally load accounts for dropdown
+            ViewBag.MaTaiKhoanList = new SelectList(
+                _context.TAIKHOAN.OrderBy(t => t.TenDangNhap),
+                "MaTaiKhoan", "TenDangNhap");
             return View();
         }
 
         // POST: HOCVIENs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MaHocVien,HoTen,SoDienThoai,NgaySinh,Email,DiaChi,MaTaiKhoan")] HOCVIEN hOCVIEN)
+        public async Task<IActionResult> Create(
+            [Bind("MaHocVien,HoTen,SoDienThoai,NgaySinh,Email,DiaChi,MaTaiKhoan")] HOCVIEN hocVien)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(hOCVIEN);
+                _context.Add(hocVien);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(hOCVIEN);
+            ViewBag.MaTaiKhoanList = new SelectList(
+                _context.TAIKHOAN.OrderBy(t => t.TenDangNhap),
+                "MaTaiKhoan", "TenDangNhap", hocVien.MaTaiKhoan);
+            return View(hocVien);
         }
 
         // GET: HOCVIENs/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var hOCVIEN = await _context.HOCVIEN.FindAsync(id);
-            if (hOCVIEN == null)
-            {
+            var hocVien = await _context.HOCVIEN.FindAsync(id);
+            if (hocVien == null)
                 return NotFound();
-            }
-            return View(hOCVIEN);
+
+            // Khởi tạo dropdown Tài khoản
+            ViewBag.MaTaiKhoanList = new SelectList(
+                _context.TAIKHOAN
+                        .AsNoTracking()
+                        .OrderBy(t => t.TenDangNhap)
+                        .ToList(),
+                "MaTaiKhoan",
+                "TenDangNhap",
+                hocVien.MaTaiKhoan
+            );
+
+            return View(hocVien);
         }
 
         // POST: HOCVIENs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("MaHocVien,HoTen,SoDienThoai,NgaySinh,Email,DiaChi,MaTaiKhoan")] HOCVIEN hOCVIEN)
+        public async Task<IActionResult> Edit(int id,
+            [Bind("MaHocVien,HoTen,SoDienThoai,NgaySinh,Email,DiaChi,MaTaiKhoan")] HOCVIEN hocVien)
         {
-            if (id != hOCVIEN.MaHocVien)
+            if (id != hocVien.MaHocVien)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                // Refill dropdown nếu có lỗi
+                ViewBag.MaTaiKhoanList = new SelectList(
+                    _context.TAIKHOAN
+                            .AsNoTracking()
+                            .OrderBy(t => t.TenDangNhap)
+                            .ToList(),
+                    "MaTaiKhoan",
+                    "TenDangNhap",
+                    hocVien.MaTaiKhoan
+                );
+                return View(hocVien);
             }
 
-            if (ModelState.IsValid)
+            try
             {
-                try
-                {
-                    _context.Update(hOCVIEN);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!HOCVIENExists(hOCVIEN.MaHocVien))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                _context.Update(hocVien);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(hOCVIEN);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.HOCVIEN.Any(e => e.MaHocVien == id))
+                    return NotFound();
+                throw;
+            }
+        }
+
+        // GET: HOCVIENs/AssignClass/5
+        public async Task<IActionResult> AssignClass(int? id)
+        {
+            if (id == null) return NotFound();
+            var hocVien = await _context.HOCVIEN.FindAsync(id);
+            if (hocVien == null) return NotFound();
+
+            ViewBag.LopHocList = new SelectList(
+                _context.LOPHOC.OrderBy(l => l.TenLop),
+                "MaLopHoc", "TenLop");
+            return View(hocVien);
+        }
+
+        // POST: HOCVIENs/AssignClass/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignClass(int id, int MaLopHoc)
+        {
+            var phieu = new PHIEUDANGKY
+            {
+                MaHocVien = id,
+                MaLopHoc = MaLopHoc,
+                NgayDangKy = DateTime.Now
+            };
+            _context.PHIEUDANGKY.Add(phieu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(DetailsWithHistory), new { id });
         }
 
         // GET: HOCVIENs/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var hOCVIEN = await _context.HOCVIEN
-                .FirstOrDefaultAsync(m => m.MaHocVien == id);
-            if (hOCVIEN == null)
-            {
-                return NotFound();
-            }
+            var hocVien = await _context.HOCVIEN
+                              .FirstOrDefaultAsync(m => m.MaHocVien == id);
+            if (hocVien == null) return NotFound();
 
-            return View(hOCVIEN);
+            return View(hocVien);
         }
 
         // POST: HOCVIENs/Delete/5
@@ -139,11 +195,9 @@ namespace QuanLyTTNgoaiNgu.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var hOCVIEN = await _context.HOCVIEN.FindAsync(id);
-            if (hOCVIEN != null)
-            {
-                _context.HOCVIEN.Remove(hOCVIEN);
-            }
+            var hocVien = await _context.HOCVIEN.FindAsync(id);
+            if (hocVien != null)
+                _context.HOCVIEN.Remove(hocVien);
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
